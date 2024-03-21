583196f8da717844e6c34d2e5dc45fa374aaa77d
Vaibhav Baria <kiboyagami0@gmail.com>
2024-03-21 17:43:15 -0400

    created Teacher Controllers and models

---
+++ Project-N01543896/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Project_N01543896.Models
+{
+    public class Teacher
+    {
+        //The following fields define a Teacher
+        public int teacherid;
+        public string teacherfname;
+        public string teacherlname;
+        public string employeenumber;
+        public DateTime hiredate;
+        public decimal salary;
+    }
+}
